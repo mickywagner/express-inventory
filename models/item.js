931ed99ba847ec0c5f8cfe2fce54687974a53f17
Michaela Wagner <mickywagner@gmail.com>
2020-04-28 13:25:53 -0600

    Define item schema

---
+++ models/item.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var ItemSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        description: {type: String, required: true},
+        rarity: {type: String, required: true, enum: ['Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'], default: 'Common'},
+        category: { type: Schema.Types.ObjectId, ref: 'Category', required: true}
+    }
+)
+
+ItemSchema
+    .virtual('url')
+    .get(function() {
+        return ('/item/' + this._id)
+    })
+
+module.exports = mongoose.model('Item', ItemSchema)
