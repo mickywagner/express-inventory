09bd64d35b3583e63009d10f313f6dd697079e4e
Michaela Wagner <mickywagner@gmail.com>
2020-04-29 19:16:10 -0600

    Item controller mock functions

---
+++ controllers/itemController.js
@@ -0,0 +1,33 @@
+var Item = require('../models/item')
+
+exports.item_list = function(req, res) {
+    res.send('FUTURE ITEM LIST')
+}
+
+exports.item_detail = function(req, res) {
+    res.send('FUTURE ITEM DETAILS')
+}
+
+exports.item_create_get = function(req, res) {
+    res.send('FUTURE CREATE ITEM GET')
+}
+
+exports.item_create_post = function(req, res) {
+    res.send('FUTURE CREATE ITEM POST')
+}
+
+exports.item_update_get = function(req, res) {
+    res.send('FUTURE UPDATE ITEM GET')
+}
+
+exports.item_update_post = function(req, res) {
+    res.send('FUTURE UPDATE ITEM POST')
+}
+
+exports.item_delete_get = function(req, res) {
+    res.send('FUTURE DELETE ITEM GET')
+}
+
+exports.item_delete_post = function(req, res) {
+    res.send('FUTURE DELETE ITEM POST')
+}
