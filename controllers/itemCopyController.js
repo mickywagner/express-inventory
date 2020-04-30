d9a58defcb96814f7c38a64445f4f423b7dc7243
Michaela Wagner <mickywagner@gmail.com>
2020-04-29 19:29:58 -0600

    Item copy mock functions

---
+++ controllers/itemCopyController.js
@@ -0,0 +1,33 @@
+var itemCopy = require('../models/itemcopy')
+
+exports.itemCopy_list = function(req, res) {
+    res.send('ITEM COPY LIST')
+}
+
+exports.itemCopy_detail = function(req, res) {
+    res.send('ITEM COPY DETAILS')
+}
+
+exports.itemCopy_create_get = function(req, res) {
+    res.send('FUTURE ITEM COPY CREATE GET')
+}
+
+exports.itemCopy_create_post = function(req, res) {
+    res.send('FUTURE ITEM COPY CREATE POST')
+}
+
+exports.itemCopy_update_get = function(req, res) {
+    res.send('FUTURE ITEM COPY UPDATE GET')
+}
+
+exports.itemCopy_update_post = function(req, res) {
+    res.send('FUTURE ITEM COPY UPDATE POST')
+}
+
+exports.itemCopy_delete_get = function(req, res) {
+    res.send('FUTURE ITEM COPY DELETE GET')
+}
+
+exports.itemCopy_delete_post = function(req, res) {
+    res.send('FUTURE ITEM COPY DELETE POST')
+}
