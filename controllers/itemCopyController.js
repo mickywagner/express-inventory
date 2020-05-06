cbad022a5d6c3f83b821e52ccae42e5a5ad73975
Michaela Wagner <mickywagner@gmail.com>
2020-05-06 13:21:48 -0600

    GET method for Update Item Copy

--- controllers/itemCopyController.js
+++ controllers/itemCopyController.js
@@ -4,6 +4,8 @@ var Item = require('../models/item')
 const { body, validationResult } = require('express-validator/check')
 const { sanitizeBody } = require('express-validator/filter')
 
+var async = require('async')
+
 exports.itemCopy_list = function(req, res, next) {
     itemCopy.find()
         .populate('item')
@@ -31,7 +33,7 @@ exports.itemCopy_create_get = function(req, res, next) {
     Item.find()
         .exec(function(err, items) {
             if(err) {return next(err)}
-            res.render('itemcopy_form', {title: 'Create New Item Copy', items: items})
+            res.render('itemcopy_form', {title: 'Create New Item Copy', items: items, selected_item: ''})
         })
 }
 
@@ -70,8 +72,18 @@ exports.itemCopy_create_post = [
     }
 ]
 
-exports.itemCopy_update_get = function(req, res) {
-    res.send('FUTURE ITEM COPY UPDATE GET')
+exports.itemCopy_update_get = function(req, res, next) {
+    async.parallel({
+        itemcopy: function(callback) {
+            itemCopy.findById(req.params.id).populate('item').exec(callback)
+        },
+        item: function(callback) {
+            Item.find().exec(callback)
+        }
+    }, function(err, results) {
+        if(err) { return next(err)}
+        res.render('itemcopy_form', { title: 'Update Item Copy', itemcopy: results.itemcopy, items: results.item, selected_item: results.itemcopy.item._id})
+    })
 }
 
 exports.itemCopy_update_post = function(req, res) {
