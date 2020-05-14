68089c6e956ef3d0e4656bf818d4947c6b6ec619
Michaela Wagner <mickywagner@gmail.com>
2020-05-14 16:47:02 -0600

    Create User model

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,9 @@ router.get('/register', function(req, res, next) {
   res.render('register')
 })
 
+router.post('/register', function(req, res, next) {
+  // create a hashed password
+  // create a user with the email and password saved to database
+})
+
 module.exports = router;
